2b0e3ff678607217a0e8f50a6055f2cb73b4fbd1
IvanLauLinTiong <ivanlau91@hotmail.com>
2024-05-25 03:41:11 +0800

    refactor code into application and router dir and add handler

--- main.go
+++ main.go
@@ -1,32 +1,50 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
+// *******************************************************************
+// import (
+// 	"fmt"
+// 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+// 	"github.com/go-chi/chi/v5"
+// 	"github.com/go-chi/chi/v5/middleware"
+// )
+
+// func main() {
+// 	router := chi.NewRouter()
+// 	router.Use(middleware.Logger)
+
+// 	router.Get("/hello", basicHandler)
+
+// 	server := &http.Server{
+// 		Addr: ":3000",
+// 		// Handler: http.HandlerFunc(basicHandler),
+// 		Handler: router,
+
+// 	}
+
+// 	err := server.ListenAndServe()
+// 	if err != nil {
+// 		fmt.Println("failed to lister server: ", err)
+// 	}
+// }
+
+// func basicHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello world!"))
+// }
+// *******************************************************************
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/IvanLauLinTiong/go-microservice/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
 
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr: ":3000",
-		// Handler: http.HandlerFunc(basicHandler),
-		Handler: router,
-
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to lister server: ", err)
+		fmt.Println("failed to start the app:", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!"))
 }
